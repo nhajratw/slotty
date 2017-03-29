6e90b4b6b9741e3b2df61525cfc8291edc31c7fa
Nayan Hajratwala <nayan@chikli.com>
2017-03-29 07:58:50 -0400

    currently borked

--- src/ScheduleTime.elm
+++ src/ScheduleTime.elm
@@ -1,23 +1,37 @@
 module ScheduleTime exposing (..)
 
+import Time exposing (inMilliseconds)
+import Date exposing (Date, Month(..), toTime)
+import Date.Extra as Date
+
 
 type alias ScheduleTime =
-    { hour : Int, minute : Int }
+    { date : Date }
 
 
-timeRange : ScheduleTime -> ScheduleTime -> List ScheduleTime
+timeRange : Date -> Date -> List Date
 timeRange startTime endTime =
-    List.map (\min -> ScheduleTime 1 min) (everyFive (minutesWithin startTime endTime))
+    List.map everyFiveMinutes (List.range (dateToMillis startTime) (dateToMillis endTime))
 
 
-minutesWithin : ScheduleTime -> ScheduleTime -> List Int
-minutesWithin startTime endTime =
-    List.range startTime.minute endTime.minute
+dateToMillis : Date -> Int
+dateToMillis date =
+    round (inMilliseconds (toTime date))
 
 
-everyFive : List Int -> List Int
-everyFive minutes =
-    List.filter (\minute -> divisibleBy5 minute) minutes
+
+-- [ Date.fromParts 1999 Dec 31 1 minute 0 0 ]
+-- List.map (\min -> ScheduleTime 1 min) (everyFive (minutesWithin startTime endTime))
+-- minutesWithin : ScheduleTime -> ScheduleTime -> List Int
+-- minutesWithin startTime endTime =
+--     List.range startTime.minute endTime.minute
+--
+--
+
+
+everyFiveMinutes : List Int -> List Int
+everyFiveMinutes milliseconds =
+    List.filter (\millesecond -> divisibleBy5 (millesecond * 1000 * 60)) milliseconds
 
 
 divisibleBy5 : Int -> Bool
--- tests/Tests.elm
+++ tests/Tests.elm
@@ -2,7 +2,9 @@ module Tests exposing (..)
 
 import Test exposing (..)
 import Expect
-import ScheduleTime exposing (ScheduleTime, timeRange, divisibleBy5)
+import ScheduleTime exposing (ScheduleTime, timeRange)
+import Date exposing (Month(..))
+import Date.Extra as Date
 
 
 all : Test
@@ -11,33 +13,40 @@ all =
         [ describe "Schedule Time"
             [ test "single value" <|
                 \() ->
-                    Expect.equal (timeRange (ScheduleTime 1 0) (ScheduleTime 1 0))
-                        [ ScheduleTime 1 0
+                    Expect.equal (timeRange (Date.fromParts 1999 Dec 31 1 0 0 0) (Date.fromParts 1999 Dec 31 1 0 0 0))
+                        [ Date.fromParts 1999 Dec 31 1 0 0 0
                         ]
             , test "dual values" <|
                 \() ->
-                    Expect.equal (timeRange (ScheduleTime 1 0) (ScheduleTime 1 5))
-                        [ ScheduleTime 1 0
-                        , ScheduleTime 1 5
+                    Expect.equal (timeRange (Date.fromParts 1999 Dec 31 1 0 0 0) (Date.fromParts 1999 Dec 31 1 5 0 0))
+                        [ Date.fromParts 1999 Dec 31 1 0 0 0
+                        , Date.fromParts 1999 Dec 31 1 5 0 0
                         ]
-            , test "multiple values" <|
-                \() ->
-                    Expect.equal (timeRange (ScheduleTime 1 0) (ScheduleTime 1 25))
-                        [ ScheduleTime 1 0
-                        , ScheduleTime 1 5
-                        , ScheduleTime 1 10
-                        , ScheduleTime 1 15
-                        , ScheduleTime 1 20
-                        , ScheduleTime 1 25
-                        ]
+              -- , test "multiple values" <|
+              --     \() ->
+              --         Expect.equal (timeRange (ScheduleTime 1 0) (ScheduleTime 1 25))
+              --             [ ScheduleTime 1 0
+              --             , ScheduleTime 1 5
+              --             , ScheduleTime 1 10
+              --             , ScheduleTime 1 15
+              --             , ScheduleTime 1 20
+              --             , ScheduleTime 1 25
+              --             ]
+              -- , test "across hour boundary" <|
+              --     \() ->
+              --         Expect.equal (timeRange (ScheduleTime 1 55) (ScheduleTime 2 5))
+              --             [ ScheduleTime 1 55
+              --             , ScheduleTime 2 0
+              --             , ScheduleTime 2 5
+              --             ]
             ]
-        , describe
-            "Divisible by 5"
-            [ test "divisible" <|
-                \() ->
-                    Expect.equal (divisibleBy5 10) True
-            , test "indivisible" <|
-                \() ->
-                    Expect.equal (divisibleBy5 7) False
-            ]
+          -- , describe
+          --     "Divisible by 5"
+          --     [ test "divisible" <|
+          --         \() ->
+          --             Expect.equal (divisibleBy5 10) True
+          --     , test "indivisible" <|
+          --         \() ->
+          --             Expect.equal (divisibleBy5 7) False
+          --     ]
         ]
