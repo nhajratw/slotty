ae9e6ae36fa75e4a517c74e3416826feac1477ed
Nayan Hajratwala <nayan@chikli.com>
2017-03-30 15:52:02 -0400

    removed unused functions;

--- src/ScheduleTime.elm
+++ src/ScheduleTime.elm
@@ -1,6 +1,5 @@
 module ScheduleTime exposing (..)
 
-import Time exposing (inMilliseconds, minute)
 import Date exposing (Date, Month(..), toTime)
 import Date.Extra as Date exposing (Interval(..))
 
@@ -9,35 +8,15 @@ type alias ScheduleTime =
     { date : Date }
 
 
+minMinuteIncrement : Int
+minMinuteIncrement =
+    5
+
+
 timeRange : Date -> Date -> List Date
 timeRange startTime endTime =
-    if (Date.diff Minute startTime endTime < 5) then
+    if (Date.diff Minute startTime endTime < minMinuteIncrement) then
         [ endTime ]
     else
         startTime
-            :: (timeRange (Date.add Minute 5 startTime) endTime)
-
-
-dateToMillis : Date -> Int
-dateToMillis date =
-    round (inMilliseconds (toTime date))
-
-
-
--- [ Date.fromParts 1999 Dec 31 1 minute 0 0 ]
--- List.map (\min -> ScheduleTime 1 min) (everyFive (minutesWithin startTime endTime))
--- minutesWithin : ScheduleTime -> ScheduleTime -> List Int
--- minutesWithin startTime endTime =
---     List.range startTime.minute endTime.minute
---
---
-
-
-everyFiveMinutes : List Int -> List Int
-everyFiveMinutes milliseconds =
-    List.filter (\millesecond -> divisibleBy5 (millesecond * 1000 * 60)) milliseconds
-
-
-divisibleBy5 : Int -> Bool
-divisibleBy5 x =
-    rem x 5 == 0
+            :: (timeRange (Date.add Minute minMinuteIncrement startTime) endTime)
