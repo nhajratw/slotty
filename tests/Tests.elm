6574ad6614f66fcdeb043e084f55efd9e828d5ba
Nayan Hajratwala <nayan@chikli.com>
2017-03-30 15:55:16 -0400

    cover additional cases

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -9,7 +9,7 @@ import Date.Extra as Date
 
 all : Test
 all =
-    describe "Sample Test Suite"
+    describe "Test Suite"
         [ describe "Schedule Time"
             [ test "single value" <|
                 \() ->
@@ -22,31 +22,22 @@ all =
                         [ Date.fromParts 1999 Dec 31 1 0 0 0
                         , Date.fromParts 1999 Dec 31 1 5 0 0
                         ]
-              -- , test "multiple values" <|
-              --     \() ->
-              --         Expect.equal (timeRange (ScheduleTime 1 0) (ScheduleTime 1 25))
-              --             [ ScheduleTime 1 0
-              --             , ScheduleTime 1 5
-              --             , ScheduleTime 1 10
-              --             , ScheduleTime 1 15
-              --             , ScheduleTime 1 20
-              --             , ScheduleTime 1 25
-              --             ]
-              -- , test "across hour boundary" <|
-              --     \() ->
-              --         Expect.equal (timeRange (ScheduleTime 1 55) (ScheduleTime 2 5))
-              --             [ ScheduleTime 1 55
-              --             , ScheduleTime 2 0
-              --             , ScheduleTime 2 5
-              --             ]
+            , test "multiple values" <|
+                \() ->
+                    Expect.equal (timeRange (Date.fromParts 1999 Dec 31 1 0 0 0) (Date.fromParts 1999 Dec 31 1 25 0 0))
+                        [ Date.fromParts 1999 Dec 31 1 0 0 0
+                        , Date.fromParts 1999 Dec 31 1 5 0 0
+                        , Date.fromParts 1999 Dec 31 1 10 0 0
+                        , Date.fromParts 1999 Dec 31 1 15 0 0
+                        , Date.fromParts 1999 Dec 31 1 20 0 0
+                        , Date.fromParts 1999 Dec 31 1 25 0 0
+                        ]
+            , test "across hour boundary" <|
+                \() ->
+                    Expect.equal (timeRange (Date.fromParts 1999 Dec 31 1 55 0 0) (Date.fromParts 1999 Dec 31 2 5 0 0))
+                        [ Date.fromParts 1999 Dec 31 1 55 0 0
+                        , Date.fromParts 1999 Dec 31 2 0 0 0
+                        , Date.fromParts 1999 Dec 31 2 5 0 0
+                        ]
             ]
-          -- , describe
-          --     "Divisible by 5"
-          --     [ test "divisible" <|
-          --         \() ->
-          --             Expect.equal (divisibleBy5 10) True
-          --     , test "indivisible" <|
-          --         \() ->
-          --             Expect.equal (divisibleBy5 7) False
-          --     ]
         ]
